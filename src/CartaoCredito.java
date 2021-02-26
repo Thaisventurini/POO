b2550e91f668c726b22d6095a1c6e3429e80e12b
Ari Thomazini <ari.thomazini@gmail.com>
2021-02-26 13:57:30 -0300

    Criando entidades de cartoes com os atributos

---
+++ src/CartaoCredito.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class CartaoCredito {
+
+    String nomeTitular;
+    double limite;
+    double saldo;
+    double fatura;
+    int cvv;
+    Date dataValidade;
+
+}
---
+++ src/CartaoDebito.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class CartaoDebito {
+
+    String nomeTitular;
+    int numero;
+    int cvv;
+    Date dataValidade;
+    
+}
